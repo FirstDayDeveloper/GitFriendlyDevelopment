d7634ecca9e47f5a925ee54a9fad8c5d82d074f4
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2022-07-27 03:12:50 +1000

    Loops

--- Loop types Playground.playground/Contents.swift
+++ Loop types Playground.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var c = [Int]()
+
+// Challenge: Add 10 numbers into the array. Duplicates are okay.
+for _ in 1...10 {
+    let randomNumber = Int.random(in: 1...10)
+    c.append(randomNumber)
+    print(randomNumber)
+}
+
+// Challenge: Add 10 numbers into the array/ Duplicates are not allowed
+c.removeAll()
+
+repeat {
+    
+    let randomNumber = Int.random(in: 1...10)
+    if c.contains(randomNumber) == false {
+// You can also use if c.contains(randomNumber) !=. This checks for if the number is already contained in the array.
+        c.append(randomNumber)
+    }
+    print(randomNumber)
+    print(c)
+} while (c.count < 10)
+
+// While loop to achieve similar results
+
+while c.count < 10 {
+    let randomNumber = Int.random(in: 1...10)
+    if c.contains(randomNumber) == false {
+        c.append(randomNumber)
+    }
+    print(randomNumber)
+    print(c)
+}
